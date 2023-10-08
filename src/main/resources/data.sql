a4083956e7ad572fcb3665bff9e7c4c11b0dd411
Ingridviieria <rm95396@fiap.com.br>
2023-10-08 20:06:33 -0300

    tabela

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PROD (produto, descricao) VALUES ('Arroz', '3 arroz de 5kg');
+INSERT INTO PROD (produto, descricao) VALUES ('Feijao', '2 pacotes');
+INSERT INTO PROD (produto, descricao) VALUES ('Bala fini', '2 caixar');
+INSERT INTO PROD (produto, descricao) VALUES ('Farinha', '1 saco');
