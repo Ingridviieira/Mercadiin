e948e724bacaee82b1530040179bace1a276d21a
Ingridviieria <rm95396@fiap.com.br>
2023-10-08 20:54:41 -0300

    tabelas atualizadas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
-INSERT INTO PROD (produto, descricao) VALUES ('Arroz', '3 arroz de 5kg');
-INSERT INTO PROD (produto, descricao) VALUES ('Feijao', '2 pacotes');
-INSERT INTO PROD (produto, descricao) VALUES ('Bala fini', '2 caixar');
-INSERT INTO PROD (produto, descricao) VALUES ('Farinha', '1 saco');
+INSERT INTO PROD (produto, descricao, score, status) VALUES ('Arroz', '3 arroz de 5kg', 20, 2);
+INSERT INTO PROD (produto, descricao, score, status) VALUES ('Feijao', '2 pacotes', 50, 5);
+INSERT INTO PROD (produto, descricao, score, status) VALUES ('Bala fini', '2 caixas', 25, 2);
+INSERT INTO PROD (produto, descricao, score, status) VALUES ('Farinha', '1 saco', 40, 4);
+
+
